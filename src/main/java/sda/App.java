1dcfd6deafb718613223542153e4b7c8244baf5b
AnitaMalek <anitamalek1991@gmail.com>
2019-02-02 11:47:17 +0100

    *

--- src/main/java/sda/App.java
+++ src/main/java/sda/App.java
@@ -12,13 +12,13 @@ public class App
     public static void main( String[] args )
     {
 
-       EmployeeList lista = new EmployeeList();
-       lista.addEmployee(new Employee("Ania", "Bed"));
-       lista.addEmployee(new Employee("Maja", "Hdhfguyg"));
-       lista.addEmployee(new Employee("Ewa", "Jhfbuy"));
-       lista.addEmployee(new Employee("Rab", "Bajdj"));
-
-        System.out.println(lista.find(2));;
+//       EmployeeList lista = new EmployeeList();
+//       lista.addEmployee(new Employee("Ania", "Bed"));
+//       lista.addEmployee(new Employee("Maja", "Hdhfguyg"));
+//       lista.addEmployee(new Employee("Ewa", "Jhfbuy"));
+//       lista.addEmployee(new Employee("Rab", "Bajdj"));
+//
+//        System.out.println(lista.find(2));;
 
 
 
